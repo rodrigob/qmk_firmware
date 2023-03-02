c1dff197cf51cc88280a025a23c1a1c5e002ab68
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2023-03-02 13:02:25 +0100

    Adds volume controls to pinky/4.

--- keyboards/pinky/4/keymaps/rodrigo2019/rules.mk
+++ keyboards/pinky/4/keymaps/rodrigo2019/rules.mk
@@ -1,2 +1,6 @@
-KEY_OVERRIDE_ENABLE = yes
-DYNAMIC_TAPPING_TERM_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes # Enable the key override feature
+
+EXTRAKEY_ENABLE  = yes  # Audio control and System control
+
+DYNAMIC_TAPPING_TERM_ENABLE = yes # Allows to configure the global tapping term on the fly.
+
